1e04c13de46bbd4de7635ffeb553476a26b01441
sazidhayatkhan <sazidhayatkhan@gmail.com>
2022-04-01 13:40:06 +0600

    code update

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     <Header/>
     <Home/>
     <Card/>
-    
+    <Footer/>
     
 
    
--- src/Home.js
+++ src/Home.js
@@ -7,6 +7,46 @@ import Card from './Card'
 
 
 function Home() {
+  const tripList =[
+    {
+      img: "./images/image card 1.png",
+      title:'Own Trip',
+      description:'Unique activities you can do yourself, led by a world of hosts. '
+    },
+    {
+      img: './images/image card 2.png',
+      title:'Group Trip',
+      description: 'Unique activities you can do yourself, led by a world of hosts'
+    },
+    {
+      img:'./images/image card 3.png',
+      title:'Custom Trip',
+      description:'Unique activities you can do yourself, led by a world of hosts'
+    }
+    
+
+  ]
+
+  const placeList=[
+    {
+      img: "./images/Bandarban.png",
+      title:'Bandarban',
+      description:'Unique activities you can do yourself, led by a world of hosts.',
+      price: '3000৳ - 5000৳'
+    },
+    {
+      img: './images/Coxbazar.png',
+      title:"Cox's Bazar",
+      description: 'Unique activities you can do yourself, led by a world of hosts',
+      price: '4000৳ - 7000৳'
+    },
+    {
+      img:'./images/Saintmartin.png',
+      title:'Saint Martin',
+      description:'Unique activities you can do yourself, led by a world of hosts',
+      price: '6000৳ - 9000৳'
+    }
+  ]
   return (
     <div className='home'>
      
@@ -14,50 +54,21 @@ function Home() {
     <Banner/>
 
     <div className='home_section'>
-      <Card
-        src='./images/image card 1.png'
-        title='Own Trip'
-        description='Unique activities you can do yourself, led by a world of hosts. '
-      />
+     {
+       tripList.map((trip) => (<Card src={trip.img} title={trip.title} description={trip.description}/> ))
+     }
+     </div>
 
-      <Card
-      src='./images/image card 2.png'
-      title='Group Trip'
-      description='Unique activities you can do yourself, led by a world of hosts'
-      />
-      <Card
-      src='./images/image card 3.png'
-      title='Custom Trip'
-      description='Unique activities you can do yourself, led by a world of hosts'
-      />
-    </div>
-
-    <div className='home_section'>
-    <Card
-        src='./images/Bandarban.png'
-        title='Bandarban'
-        description='Bandarban hill tracts steeped in isolated tradition of diverse indigenous minorities.'
-      />
-
-      <Card
-      src='./images/Coxbazar.png'
-      title='CoxsBazar'
-      description='Unique activities you can do yourself, led by a world of hosts'
-      />
-      <Card
-      src='./images/Saintmartin.png'
-      title='Saint Martin'
-      description='Unique activities you can do yourself, led by a world of hosts'
-      />
-
+     <div className='home_section'>
+      {
+        placeList.map((place)=> (<Card src={place.img} title={place.title} description={place.description} price={place.price} /> ))
+      }
+      </div>
+      
           
     </div>
     
   
-
-  
-
-    </div>
 
   )
 }
